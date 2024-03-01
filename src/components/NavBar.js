5b07f8379a5e89ca7b605dda2590b095394886a4
Mousa Khaleel <mousakhaleel22@gmail.com>
2024-03-02 01:07:29 +0200

    v0.2.4.1

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -37,62 +37,70 @@ function NavBar() {
     }
 
     return ( 
-      <nav className="navbar navbar-expand-lg navbar-dark" style={{ background: 'rgb(11, 36, 71)' }}>
-      <div className="container-fluid">
-        <a className="navbar-brand" href="/">
-          <img src={Logo} alt="ThunderBlog" width="40" />ThunderBlog
-        </a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+<nav className="navbar navbar-expand-lg navbar-dark" style={{ background: 'rgb(11, 36, 71)' }}>
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">
+      <img src={Logo} alt="ThunderBlog" width="40" />ThunderBlog
+    </a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/"><FaHome /> Home</a>
+        </li>
+        {name && (
+          <>
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/"><FaHome /> Home</a>
+              <a className="nav-link" href="/myblogs"><FaCloud /> My Blogs</a>
             </li>
-            {name && <>
-              <li className="nav-item">
-                <a className="nav-link" href="/myblogs"><FaCloud /> My Blogs</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/writeblog"><FaCloud /> Write a blog</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/aboutus"><HiIdentification /> About us</a>
-              </li>
-            </>}
-            {!name && <>
-              <li className="nav-item">
-                <a className="nav-link" href="/login"><FaCloud /> My Blogs</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/login"><FaCloud /> Write a blog</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/aboutus"><HiIdentification /> About us</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/login"><IoMdLogIn /> Login</a>
-              </li>
-            </>}
-          </ul>
-          {name && (
-            <ul className="navbar-nav">
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  <MdAccountBox /> Welcome {name}
-                </a>
-                <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
-                  <li><a className="dropdown-item" href="/login"><MdAccountCircle /> Account</a></li>
-                  <li><a className="dropdown-item" href="#"><FaHistory /> History</a></li>
-                  <li><a className="dropdown-item" onClick={handleLogout} href="/"><IoIosLogOut /> Log out</a></li>
-                </ul>
-              </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/writeblog"><FaCloud /> Write a blog</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/aboutus"><HiIdentification /> About us</a>
+            </li>
+          </>
+        )}
+        {!name && (
+          <>
+            <li className="nav-item">
+              <a className="nav-link" href="/login"><FaCloud /> My Blogs</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/login"><FaCloud /> Write a blog</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/aboutus"><HiIdentification /> About us</a>
+            </li>
+          </>
+        )}
+      </ul>
+      {name && (
+        <ul className="navbar-nav">
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <MdAccountBox /> Welcome {name}
+            </a>
+            <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
+              <li><a className="dropdown-item" href="/login"><MdAccountCircle /> Account</a></li>
+              <li><a className="dropdown-item" href="#"><FaHistory /> History</a></li>
+              <li><a className="dropdown-item" onClick={handleLogout} href="/"><IoIosLogOut /> Log out</a></li>
             </ul>
-          )}
-        </div>
-      </div>
-    </nav>
+          </li>
+        </ul>
+      )}
+      {!name && (
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="/login"><IoMdLogIn /> Login</a>
+          </li>
+        </ul>
+      )}
+    </div>
+  </div>
+</nav>
      );
 }
 
