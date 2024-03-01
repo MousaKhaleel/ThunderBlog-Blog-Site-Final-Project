98a7202740870ebb65736e7a76622d28a5e68cd0
Mousa Khaleel <mousakhaleel22@gmail.com>
2024-03-01 18:42:10 +0200

    v0.2.0.1

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,13 @@ import Logo from '../assets/9HDL7w-LogoMakr.png'
 import defaultAv from '../assets/avatar/profile-42914_1280.png'
 import $ from 'jquery'; 
 import Popper from 'popper.js'; 
+import { FaHome } from "react-icons/fa";
+import { FaCloud } from "react-icons/fa";
+import { HiIdentification } from "react-icons/hi";
+import { MdAccountCircle } from "react-icons/md";
+import { FaHistory } from "react-icons/fa";
+import { MdAccountBox } from "react-icons/md";
+import { IoIosLogOut } from "react-icons/io";
 
 
 function NavBar() {
@@ -23,25 +30,25 @@ function NavBar() {
     <div className="collapse navbar-collapse" id="navbarNavDropdown">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
+          <a className="nav-link active" aria-current="page" href="/"><FaHome /> Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">My Blogs</a>
+          <a className="nav-link" href="#"><FaCloud /> My Blogs</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/writeblog">Write a blog</a>
+          <a className="nav-link" href="/writeblog"><FaCloud /> Write a blog</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/aboutus">About us</a>
+          <a className="nav-link" href="/aboutus"><HiIdentification /> About us</a>
         </li>
         <li className="nav-item dropdown user">
           <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            User Account
+          <MdAccountBox /> User Account
           </a>
           <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a className="dropdown-item" href="/login">Account</a></li>
-            <li><a className="dropdown-item" href="#">history</a></li>
-            <li><a className="dropdown-item" href="#">Log out</a></li>
+            <li><a className="dropdown-item" href="/login"><MdAccountCircle /> Account</a></li>
+            <li><a className="dropdown-item" href="#"><FaHistory /> History</a></li>
+            <li><a className="dropdown-item" href="#"><IoIosLogOut /> Log out</a></li>
           </ul>
         </li>
       </ul>
