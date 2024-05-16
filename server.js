f8600d20ad95ab12c6e6d7f3ab4821a160508fad
Mousa Khaleel <mousakhaleel22@gmail.com>
2024-05-16 16:43:31 +0300

    v0.4.2.4

--- server.js
+++ server.js
@@ -174,7 +174,9 @@ res.json({success: true})
 })
 
 // process.env.PORT || 
-var server=app.listen(8000,function(){
+var server=app.listen(process.env.PORT || 8000,function(){
     var host=server.address().address;
     var port=server.address().port;
 });
+
+module.exports=app;
