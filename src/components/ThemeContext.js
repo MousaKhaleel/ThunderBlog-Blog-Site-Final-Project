d66b7913f16cef4af5d5beb990e42c5eeda7f3eb
Mousa Khaleel <mousakhaleel22@gmail.com>
2024-06-18 03:28:40 +0300

    Revert "bug fix"
    This reverts commit f91b697ed5738eb56c53fbb5cd5046716813b272.

--- src/components/ThemeContext.js
+++ src/components/ThemeContext.js
@@ -9,11 +9,6 @@ export const ThemeProvider = ({ children }) => {
     // const [darkTheme]=useState({bg: "rgb(18, 18, 18)", txt: 'white'})
     // ...darkTheme,
 
-    useEffect(()=>{
-      if(localStorage.getItem("theme")===null)
-    localStorage.setItem('theme', 'light');
-    },[])
-
     function toggleTheme(){
     localStorage.setItem('theme', isDark ? 'dark' : 'light');
     window.location.reload();
