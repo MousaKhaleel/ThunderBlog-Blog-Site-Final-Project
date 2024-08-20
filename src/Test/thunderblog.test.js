6b1425c1e5ecc1f3b5f36c93a12ea1c1dc10db58
Mousa Khaleel <mousakhaleel22@gmail.com>
2024-08-20 20:15:48 +0300

    t v 1.0.0

--- src/Test/thunderblog.test.js
+++ src/Test/thunderblog.test.js
@@ -174,3 +174,120 @@ describe('Add Blog Endpoint', () => {
       .expect(500);
   });
 });
+
+// describe("Get History by User ID Endpoint", () => {
+//   it("should return user's history when user ID is valid", async () => {
+
+//     const userId = "65e35a887505aabde52fb3ef";
+
+//     const userHistory = [ "6637bc2ed7701bd09f4cc677","65e4f27d5ab9f0e44af7e25d" ];
+
+//     const mockUser = {
+//       _id: userId,
+//       History: userHistory
+//     };
+
+//     jest.spyOn(userCollection, "findOne").mockResolvedValue(mockUser);
+
+//     const response = await request(app)
+//       .get(`/history/${userId}`)
+//       .expect("Content-Type", /json/)
+//       .expect(200);
+
+//     expect(response.body).toEqual(userHistory);
+//   });
+
+//   it("should return 404 when user ID is not found", async () => {
+//     const userId = "65e4f27d5ab9f0e44af7e25d";
+
+//     jest.spyOn(userCollection, "findOne").mockResolvedValue(null);
+
+//     await request(app)
+//       .get(`/history/${userId}`)
+//       .expect(404);
+//   });
+// });
+
+describe("Delete History Endpoint", () => {
+  it("should delete user history successfully", async () => {
+    
+    const userId = new ObjectId();
+
+    const userWithHistory = {
+      _id: userId,
+      History: [new ObjectId(), new ObjectId()]
+    };
+    await userCollection.insertOne(userWithHistory);
+
+    const response = await request(app)
+      .delete(`/deletehistory/${userId}`)
+      .expect("Content-Type", /json/)
+      .expect(200);
+
+    expect(response.body).toEqual({ success: true });
+
+    const updatedUser = await userCollection.findOne({ _id: userId });
+    expect(updatedUser.History).toEqual([]);
+  });
+});
+
+describe("Change Password Endpoint", () => {
+  it("should change user password successfully", async () => {
+    
+    const userId = new ObjectId();
+    const newPassword = "newPassword123";
+
+    const userWithPassword = {
+      _id: userId,
+      Password: "oldPassword123" 
+    };
+    await userCollection.insertOne(userWithPassword);
+
+   
+    const response = await request(app)
+      .post("/changePassword")
+      .send({ userId: userId, password: newPassword })
+      .expect("Content-Type", /text/)
+      .expect(200);
+
+    expect(response.text).toEqual("password changed successfully");
+
+    const updatedUser = await userCollection.findOne({ _id: userId });
+    expect(updatedUser.Password).toEqual(newPassword);
+  });
+});
+
+describe('Recommended Blogs Endpoint', () => {
+  
+  it('should return recommended blogs based on top tags', async () => {
+
+    const sampleBlogs = [
+      { title: 'Blog 1', content: 'Content 1', preview: 'Preview 1', AuthorID: 'author1', Tags: ['tag11', 'tag2'] },
+      { title: 'Blog 2', content: 'Content 2', preview: 'Preview 2', AuthorID: 'author2', Tags: ['tag1', 'tag3'] },
+      { title: 'Blog 3', content: 'Content 3', preview: 'Preview 3', AuthorID: 'author3', Tags: ['tag2', 'tag3'] }
+    ];
+    await blogCollection.insertMany(sampleBlogs);
+
+
+    const response = await request(app)
+      .post('/recommendedblogs')
+      .send({ topTags: ['tag11'] })
+      .expect('Content-Type', /json/)
+      .expect(200);
+
+    // expect(response.body).toHaveLength(1);
+    expect(response.body[0].title).toBe('Blog 1');
+  });
+
+  it('should return an empty array if no blogs are found with top tags', async () => {
+    
+    const response = await request(app)
+      .post('/recommendedblogs')
+      .send({ topTags: ['nonexistenttag'] })
+      .expect('Content-Type', /json/)
+      .expect(200);
+
+  
+    expect(response.body).toHaveLength(0);
+  });
+});
